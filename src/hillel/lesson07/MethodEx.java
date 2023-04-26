365957f1312a8f8d1ec86849c24772ba1f9d8bda
User <slaven90@ukr.net>
2023-04-26 22:15:25 +0300

    Lesson 7

---
+++ src/hillel/lesson07/MethodEx.java
@@ -0,0 +1,16 @@
+//package hillel.lesson07;
+//
+//public class MethodEx {
+//    public static void main(String[] args) {
+//        MethodLib myMethlib = new MethodLib();
+//
+//        new MethodLib().methodWithParams
+//    }
+//
+//    int input = 10;
+//
+//    int output = MyMethLib.convertInt(input); {
+//        System.out.println(input + "")
+//    }
+//
+//}
