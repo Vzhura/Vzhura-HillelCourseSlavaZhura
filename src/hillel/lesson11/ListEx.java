5fabaff291ac58e38c06dbc0f9671704f9a06766
User <slaven90@ukr.net>
2023-05-11 21:59:24 +0300

    lesson 11

---
+++ src/hillel/lesson11/ListEx.java
@@ -0,0 +1,17 @@
+package hillel.lesson11;
+
+import java.util.ArrayList;
+import java.util.List;
+public class ListEx {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.get(1);
+
+        
+    }
+}
