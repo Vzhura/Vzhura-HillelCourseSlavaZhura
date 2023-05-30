e4a00cd8f9d061a339d0d0d43dc945e10d9cce04
User <slaven90@ukr.net>
2023-05-30 23:05:10 +0300

    Lesson 15-1

---
+++ src/test/java/ua/hillel/DataProviderTest.java
@@ -0,0 +1,37 @@
+package ua.hillel;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderTest {
+    @Test(dataProvider = "userProvider")
+    public void userRoleTest(String userName, String password, String role) {
+        String[] roles = {"user", "admin", "guest"};
+System.out.printf("name: %s - password: %s - role: %s%n",userName, password, role);
+            System.out.println("Starting test for " + role);
+            if (roles.equals("admin")) {
+                throw new RuntimeException("Test is failed");
+            }
+            System.out.println("Test for " + role + " passed");
+
+        Assert.assertEquals("actual",1, "Assertion message");
+        Assert.assertFalse(true, "Message");
+
+//        SoftAssert softUserAssert = new SoftAssert();
+//        softUserAssert.assertEquals(username, "John");
+//        softUserAssert.assertTrue(role.isBlank());
+//
+//        System.out.println("Test goes on");
+//        softUserAssert.assertAll();
+        }
+@DataProvider(name = "userProvider")
+    public Object[] [] userProvider() {
+        String[] [] users = {
+            {"userName","userPass","user"},
+            {"adminName","adminPass","admin"},
+            {"guestName","guestPass","guest"},
+        };
+        return users;
+    }
+}
